965745feee84012b9ef8f520dbb4211079376f83
MrWilson21 <33751742+MrWilson21@users.noreply.github.com>
2017-12-05 10:36:02 +0000

    Add files via upload

--- CourseWork 1/hotelSite.py
+++ CourseWork 1/hotelSite.py
@@ -1,17 +1,7 @@
 from flask import Flask, render_template
-from flask import request, redirect, session
-
-from datetime import datetime
+from flask import request
 import csv
 app = Flask(__name__)
-
-#TODO: Make Review use datetime object instead of string, maybe
-class Review():
-	def __init__(self, name, date, content):
-		self.name = name
-		self.date = date
-		self.content = content
-
 
 #Define pages --------------------------------------
 @app.route('/')
@@ -25,35 +15,10 @@ def attractions():
 @app.route('/bookings')
 def bookings(): 
     return render_template('bookings.html')
-	
-@app.route('/addBooking', methods=['post'])
-def addBooking():
-	name = request.form['name']
-	email = request.form['email']
-	arrivalDateDateTime = datetime.strptime(request.form['arrivalDate'], "%Y-%m-%d")
-	arrivalDate = datetime.strftime(arrivalDateDateTime, "%A %d/%m/%Y")
-	
-	departureDateDateTime = datetime.strptime(request.form['departureDate'], "%Y-%m-%d")
-	departureDate = datetime.strftime(departureDateDateTime, "%A %d/%m/%Y")
-	
-	writeCsv('unconfirmedBookings.csv', [name,email,arrivalDate,departureDate], 'a')
-	return redirect('/bookings')
-
-#TODO: Possibly sanitize from code injection attacks
-@app.route('/reviews', methods=['get'])
-def reviews():
-	rows = readCsv('reviews.csv')
-	reviews = [Review(n[0], n[1], n[2]) for n in rows]
-	#reviews.csv is in oldest first order
-	return render_template('reviews.html', reviews=reversed(reviews))	
-
-@app.route('/addReview', methods=['post'])
-def addReview():
-	name = request.form['name']
-	date = datetime.strftime(datetime.now(), "%A %d/%m/%Y %H:%M:%S")
-	content = request.form['content']
-	writeCsv('reviews.csv', [name, date, content], 'a')
-	return redirect('/reviews')
+    
+@app.route('/reviews')
+def reviews(): 
+    return render_template('reviews.html')
 	
 #--------------------------------------------------- 
 
@@ -69,10 +34,10 @@ def readCsv(fileName):
 		
 	return aList
 	
-def writeCsv(fileName, listName, mode):
-	with open(fileName, mode, newline='') as outFile:
+def writeCsv(fileName, listName):
+	with open(fileName, 'w', newline='') as outFile:
 		writer = csv.writer(outFile)
-		writer.writerow(listName)
+		writer.writerows(listName)
 	return
 #---------------------------------------------------
 
